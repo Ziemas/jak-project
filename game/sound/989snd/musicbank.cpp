52bf0f3c8b51e7af04e811cdf35fd4284b73f82b
Ziemas <ziemas@ziemas.se>
2022-03-30 15:29:30 +0200

    Stop carrying around useless data

--- game/sound/989snd/musicbank.cpp
+++ game/sound/989snd/musicbank.cpp
@@ -14,11 +14,11 @@ std::unique_ptr<sound_handler> MusicBank::make_handler(synth& synth,
   if (sound.Type == 4) {  // midi
     auto midi = static_cast<MIDIBlockHeader*>(m_locator.get_midi(sound.MIDIID));
     handler = std::make_unique<midi_handler>(midi, synth, (sound.Vol * vol) >> 10, pan,
-                                             sound.Repeats, sound.VolGroup, m_locator, d.BankID);
+                                             sound.Repeats, sound.VolGroup, m_locator, bank_id);
   } else if (sound.Type == 5) {  // ame
     auto midi = static_cast<MultiMIDIBlockHeader*>(m_locator.get_midi(sound.MIDIID));
     handler = std::make_unique<ame_handler>(midi, synth, (sound.Vol * vol) >> 10, pan,
-                                             sound.Repeats, sound.VolGroup, m_locator, d.BankID);
+                                             sound.Repeats, sound.VolGroup, m_locator, bank_id);
   } else {
     // error
   }
