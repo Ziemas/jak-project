4b9dd3b187f5ca3a94747722315ba588499ee0e9
Ziemas <ziemas@ziemas.se>
2022-04-06 02:27:58 +0200

    sfx shitshow

--- game/sound/989snd/blocksound_handler.cpp
+++ game/sound/989snd/blocksound_handler.cpp
@@ -5,14 +5,29 @@ void blocksound_handler::init() {
   m_next_grain = 0;
   m_countdown = m_sfx.grains[0].Delay;
 
+  if (m_sfx.d.Flags & 2) {
+    fmt::print("solo flag\n");
+    m_done = true;
+    return;
+  }
+
   if (!m_countdown) {
     do_grain();
   }
 }
 
 bool blocksound_handler::tick() {
-  if (m_done)
-    return m_done;
+  m_voices.remove_if([](auto& p) { return p.expired(); });
+  if (m_done) {
+    if (m_voices.empty()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  if (m_paused)
+    return false;
 
   m_countdown--;
   if (m_countdown <= 0) {
@@ -22,18 +37,49 @@ bool blocksound_handler::tick() {
   return m_done;
 };
 
-void blocksound_handler::pause() {}
-void blocksound_handler::unpause() {}
+void blocksound_handler::pause() {
+  m_paused = true;
+
+  for (auto& p : m_voices) {
+    auto voice = p.lock();
+    if (voice == nullptr) {
+      continue;
+    }
+
+    m_vm.pause(voice);
+  }
+}
+
+void blocksound_handler::unpause() {
+  m_paused = false;
+
+  for (auto& p : m_voices) {
+    auto voice = p.lock();
+    if (voice == nullptr) {
+      continue;
+    }
+
+    m_vm.unpause(voice);
+  }
+}
 
 void blocksound_handler::do_grain() {
   auto& grain = m_sfx.grains[m_next_grain];
 
   if (grain.Type == 1) {
-    // TODO passing m_pan here makes stuff sound bad, why?
-    //      auto volume = make_volume(127, m_volume, m_pan, 127,
-    //                                0, grain.GrainParams.tone.Vol, grain.GrainParams.tone.Pan);
+    auto voice = std::make_shared<vag_voice>(grain.GrainParams.tone);
 
-    // fmt::print("{:x}\n", grain.GrainParams.tone.VAGInSR);
+    auto volume = m_vm.make_volume(127, m_volume, m_pan, 127, 0, grain.GrainParams.tone.Vol,
+                                   grain.GrainParams.tone.Pan);
+
+    voice->basevol = volume;
+
+    voice->start_note = m_note;
+    voice->start_fine = m_fine;
+    voice->group = m_group;
+
+    m_vm.start_tone(voice);
+    m_voices.emplace_front(voice);
   } else {
     fmt::print("Ignoring grain {}, type {}\n", m_next_grain, grain.Type);
   }
